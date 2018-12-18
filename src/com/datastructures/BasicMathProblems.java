6439fdd8ce78078d7580f0a993503dba4564f4e6
ibmirza <Hyderabad786!>
2018-12-17 23:19:34 -0800

    Added test methods

---
+++ src/com/datastructures/BasicMathProblems.java
@@ -0,0 +1,37 @@
+package com.datastructures;
+
+public class BasicMathProblems {
+
+    public void squareRoot(){
+
+    }
+
+    public void isEven(){
+
+    }
+
+    public void isOdd(){
+
+    }
+
+    public void isPrime(){
+
+    }
+
+    public void factorial(){
+
+    }
+
+    public void fibonacci(){
+
+    }
+
+    public void palindrome(){
+
+    }
+
+    public void intToHex(){
+
+    }
+
+}
---
+++ src/com/datastructures/BasicRecursion.java
@@ -0,0 +1,46 @@
+package com.datastructures;
+
+public class BasicRecursion {
+
+    //NOTE: Do not use loops. Use recursion only
+
+    // Print N to 1
+    public void printNto1(int[] array){
+
+    }
+
+    // Print 1 to N ascending
+    public void printIToN(int[] array){
+
+    }
+
+    // Find sum of n elements of an array
+    public void sumOfDigits(int[] array){
+
+    }
+
+    //Is Array Palindromic
+    public void isArrayPalindromic(int[] array){
+
+    }
+
+    // Return index of element 'k' in an array
+    public void getIndex(int[] array, int k){
+
+    }
+
+    //Factorial of N
+    public void factorial(int n){
+
+    }
+
+    //Total fingers in a room
+    public void totalFingers(){
+
+    }
+
+
+}
+
+
+
