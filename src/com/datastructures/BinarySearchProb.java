10efbb6481da5ea694467ecc1d31a2dd3e852ed6
ibmirza <Hyderabad786!>
2018-12-21 00:43:05 -0800

    implemented logn problems

---
+++ src/com/datastructures/BinarySearchProb.java
@@ -0,0 +1,183 @@
+package com.datastructures;
+
+public class BinarySearchProb {
+
+    //find the index of an element 'k' in an array - iterative
+    public int findIndexOfElementIterative(int[] array, int k){
+    	int low = 0;
+    	int arrayLength = array.length;
+    	int high = arrayLength - 1;
+    	while(low <= high){
+    		int mid = low + (high-low)/2;
+    		if(array[mid] == k)
+    			return mid;
+    		else if(array[mid] > k)
+    			high = mid-1;
+    		else 
+    			low = mid+1;
+    	}
+        return -1;
+   }
+
+   //find the index of element 'k' in an array - recursive
+   public int findIndexOfElementRecursive(int[] array, int k){
+   	    int low = 0;
+    	int arrayLength = array.length;
+    	int high = arrayLength - 1;
+        return binarySearch(array, 0, high, k);
+   }
+
+   private int binarySearch(int[] array, int low, int high, int k){
+    	if(low<=high){
+    		int mid = low + (high - low)/2;
+    		if(array[mid] == k)
+    			return mid;
+    		else if(array[mid]<k)
+    			return binarySearch(array, mid+1, high, k);
+    		else
+    			return binarySearch(array, low, mid-1, k);
+    	}
+    	return -1;
+   }
+
+    // Print all the subsets of a given integer array
+    public void printAllSubsets(int[] array){
+
+    }
+
+    //Given a sorted array, return i index such that a[i]==i  [-5,-3,0,2,4,6,9] in 'log n' time
+    public int findIndexMatchingValue(int[] array){
+        //int p = -1;
+        int low = 0;
+    	int arrayLength = array.length;
+    	int high = arrayLength - 1;
+    	while(low <= high){
+    		int mid = low + (high-low)/2;
+    		if(array[mid] == mid){
+    			return mid;
+    		}
+    		else if(array[mid] > mid)
+    			high = mid-1;
+    		else 
+    			low = mid+1;
+    	}
+        return -1;
+    }
+
+    // Find an element 'k' in a sorted rotated array in 'log n' time
+    public void findElementInSortedRotatedArray(int[] array, int k){
+
+    }
+
+    // Find first index/occurence of element 'k' in sorted array with duplicates.  in 'log n' time
+    public int findFirstOccurence(int[] array, int k){
+        int p = -1;
+        int low = 0;
+    	int arrayLength = array.length;
+    	int high = arrayLength - 1;
+    	while(low <= high){
+    		int mid = low + (high-low)/2;
+    		if(array[mid] == k){
+    			p = mid;
+                high = mid-1;
+    		}
+    		else if(array[mid] > k)
+    			high = mid-1;
+    		else
+    			low = mid+1;
+    	}
+        return p;
+    }
+
+    //Find last index of element in sorted array with duplicates.  in 'log n' time
+    public int findLastOccurence(int[] array, int k){
+        int p = -1;
+        int low = 0;
+    	int arrayLength = array.length;
+    	int high = arrayLength - 1;
+    	while(low <= high){
+    		int mid = low + (high-low)/2;
+    		if(array[mid] == k){
+    			p = mid;
+                low = mid+1;
+    		}
+    		else if(array[mid] > k)
+    			high = mid-1;
+    		else
+    			low = mid+1;
+    	}
+        return p;
+    }
+
+    //Find frequency of element 'k' in sorted array.  in 'log n' time
+    public int findFrequency(int[] array, int k){
+        int last = findLastOccurence(array, k);
+        int first = findFirstOccurence(array, k);
+        int result = last - first;
+        if(last==-1 && first==-1)
+            return 0;
+        else if(first==last && result>=0)
+            return  first;
+        else
+            return result+1;
+    }
+
+    // Find square root of number.  in 'log n' time
+    public double findSquareRoot(double n){
+    	double low = 0;
+    	double high = n;
+    	while(low <= high){
+    		double mid = (low + high)/2.0;
+    		if(Math.round( (mid*mid) * 1000.0 ) / 1000.0 == n){
+    			return Math.round( mid * 1000.0 ) / 1000.0;
+    		}
+    		else if(mid*mid > n)
+    			high = mid;
+    		else 
+    			low = mid;
+    	}
+        return 0.0;
+    }
+
+    //Given an unsorted array of positive numbers. All zeroes moved to front. Count number of zeroes/last.
+	// Ex: [0,0,0,0,3,2,2,7,6]
+    public int zeroCount(int[] array){
+    	int p = -1;
+        int low = 0;
+    	int arrayLength = array.length;
+    	int high = arrayLength - 1;
+    	while(low <= high){
+    		int mid = low + (high-low)/2;
+    		if(array[mid] == 0 && array[mid+1]>0){
+    			return mid;
+    		}
+    		else if(array[mid] > 0)
+    			high = mid-1;
+    		else
+    			low = mid+1;
+    	}
+        return -1;
+    }
+
+    //Find the peak element in array -   log n based approach
+    // peak element is greater than its neighbors
+    public void peakElement(int[] array){
+
+    }
+
+    // Find the median of two sorted arrays without merging them log(m+n)
+    public void medianOfTwoSortedArrays(int[] a, int[] b){
+
+    }
+
+
+    // Given a sorted array with missing elements in it. Find the missing element in log n time
+    public void findMissingElement(int[] array){
+
+    }
+
+    //Given a sorted array, find the index at which the the value of 'k' should be inserted
+    public void findIndexToInsert(int[] array, int k){
+
+    }
+}
---
+++ test/com/datastructures/BinarySearchProbTests.java
@@ -0,0 +1,79 @@
+package com.datastructures;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinarySearchProbTests {
+
+    BinarySearchProb bs;
+
+    @Before
+    public void setUp() throws Exception {
+      bs = new BinarySearchProb();
+    }
+
+    @Test
+    public void testBinarySearchIterative(){
+        int[] array = {2,7};
+        Assert.assertEquals(-1, bs.findIndexOfElementIterative(array, 7));
+    }
+
+    @Test
+    public void testBinarySearchRecursive(){
+        int[] array = {1,2,3};
+        Assert.assertEquals(-1, bs.findIndexOfElementRecursive(array, 0));
+    }
+
+    @Test
+    public void testFirstOccurenceOfElement(){
+        int[] array = {1,3,4,6,6,6,8,10,10,15,30};
+        Assert.assertEquals(7, bs.findFirstOccurence(array, 10));
+        Assert.assertEquals(3, bs.findFirstOccurence(array, 6));
+        Assert.assertEquals(-1, bs.findFirstOccurence(array, 11));
+    }
+
+    @Test
+    public void testLastOccurenceOfElement(){
+        int[] array = {1,3,4,6,6,6,8,10,10,15,30};
+        Assert.assertEquals(8, bs.findLastOccurence(array, 10));
+        Assert.assertEquals(5, bs.findLastOccurence(array, 6));
+        Assert.assertEquals(-1, bs.findLastOccurence(array, 11));
+    }
+
+    @Test
+    public void testFrequencyOfElement(){
+        int[] array = {1,3,4,6,6,6,8,10,10,15,30};
+        Assert.assertEquals(2, bs.findFrequency(array, 10));
+        Assert.assertEquals(3, bs.findFrequency(array, 6));
+        Assert.assertEquals(9, bs.findFrequency(array, 15));
+        Assert.assertEquals(0, bs.findFrequency(array, 11));
+    }
+
+    @Test
+    public void testIndexWhichHasSameValue(){
+        int[] array = {-1,0,2,2,4,5};
+        int[] arr = {-1,0,1,2,3,4};
+        Assert.assertEquals(2, bs.findIndexMatchingValue(array));
+        Assert.assertEquals(-1, bs.findIndexMatchingValue(arr));
+    }
+
+    @Test
+    public void testSquareRoot(){
+       Assert.assertEquals(5.0, bs.findSquareRoot(25), 0);
+       Assert.assertEquals(2.236,bs.findSquareRoot(5), 0);
+       // double s = bs.findSquareRoot(5);
+       // System.out.println(s);
+    }
+
+    @Test
+    public void testZeroCountUnsortedArray(){
+        int[] array = {0,0,0,0,3,2,2,7,6};
+        Assert.assertEquals(3,bs.zeroCount(array));
+    }
+
+    @Test
+    public void testPeakElement(){
+
+    }
+}
